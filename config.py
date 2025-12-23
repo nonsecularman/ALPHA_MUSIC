7aa7196045bbba552a931abe0b85bd34a732e03b
krishbharti3404-source <krishbharti3404@gmail.com>
2025-12-23 20:55:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,8 +24,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/krishbharti3404-sour
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/PURVI_BOTS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PURVI_UPDATES")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/NexaCoders")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/kryshupdate")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
